601faada176919142098241ba0c118cb00db469c
Alon Daks <alondaks@airbears2-10-142-7-45.airbears2.1918.berkeley.edu>
2014-02-22 13:45:44 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ProjectsThatWork::Application.config.secret_key_base = 'e0c1000a2ff9f1a0099a726b6e129d5295e2e74ebc919b97a94161e23e33b1b6b3a03f6bf815d104702b13cba75d2714a2f1d4f994f57b11ce4102c994d934b3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ProjectsThatWork::Application.config.session_store :cookie_store, key: '_projects-that-work_session'
