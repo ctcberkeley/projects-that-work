f0ff26d136a0bd9c011b074f6679aa4ee6f85638
saasbook <simon.c312@gmail.com>
2014-04-10 18:30:46 -0300

    link to home page works small commit

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+	def nav_link(link_text, link_path, class_name)
+ 		class_named = class_name
+
+ 		content_tag(:li, :class => class_named) do
+ 			link_to link_text, link_path
+		end
+	end
 end
